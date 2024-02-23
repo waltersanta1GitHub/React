144abfcb7e5169851d5bcabf8dbd3f857c21779e
Jose Walter Santamaria <waltersanta1@hotmail.com>
2024-02-23 10:57:05 +0100

    finaliza juego dados (#1)

---
+++ dice-game-project/src/components/StartGame.jsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+max-width: 1180px;
+height: 100vh;
+display: flex;
+margin: 0 auto;
+align-items: center;
+
+.content{
+    h1 {
+        font-size: 96px;
+        white-space: nowrap;
+
+
+    }
+}
+
+`;
+
+const Button = styled.button`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+color: white;
+padding: 10px 18px;
+background: #000000;
+border-radius: 5px;
+min-width: 220px;
+border: none;
+font-size: 16px;
+border: 1px solid transparent;
+transition: 0.4s background easy-in;
+
+&:hover{
+    background-color: white;
+    border: 1px solid black;
+    color: black;
+    transition: 0.3s background easy-in
+}
+
+`;
+
+const StartGame = ({toggle}) =>{
+    return (
+        <Container>
+            <div>
+                <img  src='/images/dices.png' />
+            </div>            
+            <div className='content'>
+                <h1>Dice Game</h1>
+                <Button
+                onClick={toggle}
+                >Play Now</Button>
+            </div>            
+        </Container>
+    );
+}
+
+export default StartGame

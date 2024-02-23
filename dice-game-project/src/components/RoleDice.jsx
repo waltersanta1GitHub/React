6263ebdf2c6e9cbe1bae11064ce16ce80fbf88e0
Jose Walter Santamaria <waltersanta1@hotmail.com>
2024-02-23 10:54:14 +0100

    finaliza juego dados

---
+++ dice-game-project/src/components/RoleDice.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const RoleDice = ({ roleDice, currentDice }) => {
+  return (
+    <DiceContainer>
+      <div className="dice" onClick={roleDice}>
+        <img src={`../public/images/diceside/dice_${currentDice}.png`} alt="dice 1" />
+      </div>
+      <p>Click on Dice to roll</p>
+    </DiceContainer>
+  );
+};
+
+const DiceContainer =  styled.div`
+margin-top: 48px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .dice {
+    cursor: pointer;
+  }
+
+  p {
+    font-size: 24px;
+  }
+` ;
+
+
+
+export default RoleDice;
